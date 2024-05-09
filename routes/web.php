c6253007719d68188594323252049436e7cdd4c6
Lkaetsu <leonardo.kaetsu9@gmail.com>
2024-05-09 09:35:00 -0300

    Deps Setup 0.3
    # Changes:
    
    - Made plenty of small changes to make inertiajs and vue properly work
    - Changed the node container port to 5173 to fix a issue with vite

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('app');
+    return Inertia::render('ExampleComponent');
 });
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: { 
+        hmr: {
+            host: '127.0.0.1',
+        },
+    }, 
     plugins: [
         laravel({
             input: [
