bb1443bb75d1a19b6797b8bae65069ecd3bb6af6
EduardoSilvaS <eduardoss2020.ve@gmail.com>
2024-05-20 21:49:51 -0300

    Update contact form UI styling and labels

--- resources/js/Pages/Contact.vue
+++ resources/js/Pages/Contact.vue
@@ -1,27 +1,41 @@
 <template>
-  <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Entre em Contato</h2>
+  <div class="flex justify-center items-center min-h-screen">
+    <div class="w-1/2">
+      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Entre em Contato</h2>
+      </div>
+      <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form @submit.prevent="sendEmail" class="space-y-6">
+          <div>
+            <!-- <label for="to">Para:</label>
+            <input type="email" v-model="email.to" value="irineu0078@gmail.com" required> -->
+          </div>
+          <div>
+            <label for="subject" class="block text-sm font-medium leading-6 text-gray-900">Assunto</label>
+            <input type="text" v-model="email.subject" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          </div>
+          <div>
+            <label for="message" class="block text-sm font-medium leading-6 text-gray-900">Menssagem</label>
+            <textarea v-model="email.message" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+          </div>
+          <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            Enviar
+          </button>
+        </form>
+        <p v-if="message">{{ message }}</p>
+      </div>
     </div>
-    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form @submit.prevent="sendEmail" class="space-y-6">
-        <div>
-          <!-- <label for="to">Para:</label>
-          <input type="email" v-model="email.to" value="irineu0078@gmail.com" required> -->
-        </div>
-        <div>
-          <label for="subject" class="block text-sm font-medium leading-6 text-gray-900">Assunto</label>
-          <input type="text" v-model="email.subject" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-        </div>
-        <div>
-          <label for="message" class="block text-sm font-medium leading-6 text-gray-900">Menssagem</label>
-          <textarea v-model="email.message" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
-        </div>
-        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Enviar
-        </button>
-      </form>
-      <p v-if="message">{{ message }}</p>
+    <div class="w-1/2"> 
+      <table>
+        <tr>
+          <td>Email:</td>
+          <td>sistemaclinica@bol.com.br</td>
+        </tr>
+        <tr>
+          <td>Telefone:</td>
+          <td>(11) 99999-9999</td>
+        </tr> 
+      </table>
     </div>
   </div>
 </template>
