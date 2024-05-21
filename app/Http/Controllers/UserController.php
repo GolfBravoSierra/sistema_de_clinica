030262fddc48891081a18dcdca7bff1cd315bd10
GolfBravoSierra <106974254+GolfBravoSierra@users.noreply.github.com>
2024-05-21 13:07:38 -0300

    user controller
    primeiros passo para configurar o login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        return Inertia::render('login');
+    }
+
+    public function login(Request $request)
+    {
+
+        $user = DB::table('users')
+                ->where('name', '=', $request->name)
+                ->where('password', '=', $request->password)
+                ->findOrFail();
+        ddd($user);
+
+        return Inertia::render('home');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {return Inertia::render('Index');});
-Route::get('/login', function () {return Inertia::render('Login');});
+Route::get('/login', [UserController::class, 'login']);
 Route::get('/contact', function () {return Inertia::render('Contact');});
 Route::get('/patients', function () {return Inertia::render('Patients');});
 Route::get('/about', function () {return Inertia::render('About');});
